25ff77a0f203a8e42b332eb0ae883e199f32b6c8
gsweeh <gsweeh@gmail.com>
2024-04-05 13:08:46 +0530

    yo331

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import('node-fetch').then(nodeFetch => {
     async function makeAPICall() {
         try {
             // Make request to the endpoint
-            const response = await fetch('yourendpoint');
+            let response = await fetch('https://email.gubhai.eu.org/alert');
 
             // Check if response is successful
             if (!response.ok) {
@@ -14,7 +14,19 @@ import('node-fetch').then(nodeFetch => {
             }
 
             // Read response body as text
-            const responseBody = await response.text();
+            let responseBody = await response.text();
+            console.log('API call success. Response:', responseBody);
+
+            // 2nd
+            response = await fetch('https://email.gubhai.eu.org/alert');
+
+            // Check if response is successful
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            // Read response body as text
+            responseBody = await response.text();
             console.log('API call success. Response:', responseBody);
 
             // Calculate next delay (24 hours + additional 10 minutes)
